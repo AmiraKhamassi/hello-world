b3c7ae353f404a53a28367ebbdfb98a39cff6478
moez <moez@192.168.1.52>
2020-01-15 18:24:16 +0100

    COMMIT PROJECT

---
+++ src/javaExo/Hello.java
@@ -0,0 +1,32 @@
+package javaExo;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+		
+		Stream.of("a1", "a2", "a3")
+		.map(s -> s.substring(1))
+		.collect(Collectors.toList())
+		.forEach(System.out::println);
+		
+		Arrays.asList("a1", "a2", "a3")
+			.stream()
+			.filter(s -> s.startsWith("a"))
+			.map(s -> s.toUpperCase())
+			.forEach(System.out::println);
+		
+		try {
+			
+		}
+		catch(Exception e) {
+			System.out.println();
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author moez
+ *
+ */
+module java {
+}
